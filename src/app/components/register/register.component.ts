d1f328a05a61e689c0f25f9a10d5d17d17e6739d
Awais Kamran <awaiskamran@ITs-MacBook-Pro-4.local>
2022-02-13 14:38:57 +0500

    CU-21v2mev - feat(Register Module) Replace name with firstname and lastname

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
+import { NotificationService } from "@app/utility/notification.service";
 import { StorageService } from "@app/utility/storage.service";
 import { CATEGORIES } from "./register.constant";
 import { RegisterResponse, User } from "./register.interface";
@@ -17,12 +18,14 @@ export class RegisterComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private registerService: RegisterService
+    private registerService: RegisterService,
+    private notificationService: NotificationService
   ) {}
 
   ngOnInit(): void {
     this.registerForm = new FormGroup({
-      name: new FormControl("", [Validators.required]),
+      firstName: new FormControl("", [Validators.required]),
+      lastName: new FormControl("", [Validators.required]),
       email: new FormControl("", [Validators.required]),
       password: new FormControl("", [Validators.required]),
       confirmPassword: new FormControl("", [Validators.required]),
@@ -33,7 +36,9 @@ export class RegisterComponent implements OnInit {
     const user: User = this.registerForm?.value;
     this.registerService.postUser(user).subscribe(
       (result: RegisterResponse) => {
-        console.log(result);
+        this.notificationService.showSuccess(result);
+        this.registerForm.reset();
+        this.navigateToLogin();
       },
       (error) => console.error(error)
     );
